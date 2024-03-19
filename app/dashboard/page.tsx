e87831d43961451fc3527b80f20d9d1c2a19db4b
Federico <redolfofederico@gmail.com>
2024-03-19 19:14:47 -0300

    Add fix the bug new

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from "../ui/dashboard/revenue-chart"
 import LatestInvoices from "../ui/dashboard/latest-invoices"
 import { lusitana } from "../ui/fonts"
 import { Suspense } from "react"
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons"
+import { CardsSkeleton } from "../ui/skeletons"
 
 export default async function Dashboard() {
     
@@ -13,10 +13,10 @@ export default async function Dashboard() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardsSkeleton />}>
-                    <CardWrapper />
-                </Suspense>
-            {/*
+                {/*<Suspense fallback={<CardsSkeleton />}>
+                </Suspense>*/}
+                <CardWrapper />
+                {/*
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
