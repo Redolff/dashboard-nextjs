d38452481c2b801ecee006fe21a81e189adde206
Federico <redolfofederico@gmail.com>
2024-03-19 16:15:41 -0300

    Fix the bugs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,8 +11,9 @@ export default async function Dashboard() {
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}> 
                 Dashboard
-            </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            </h1>{/*
+
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
@@ -25,6 +26,7 @@ export default async function Dashboard() {
                     <LatestInvoices /> 
                 </Suspense>
             </div>
+            */}
         </main>
     )
 }
