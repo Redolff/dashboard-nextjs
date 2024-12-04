87cc5d27c07b9ad24bcc77135485c388a27d36d4
Federico Redolfo <redolfofederico@gmail.com>
2024-12-04 18:42:07 -0300

    recuperity password and user

--- auth.ts
+++ auth.ts
@@ -30,8 +30,8 @@ export const { auth, signIn, signOut } = NextAuth({
                     const user = await getUser(email)
                     if(!user) return null;
                     
-                    const passwordsMatch = await bcrypt.compare(password, user.password)
-                    if(passwordsMatch) return user;
+                    //const passwordsMatch = await bcrypt.compare(password, user.password)
+                    if(user) return user;
                 }
 
                 console.log('Invalidated credentials')
